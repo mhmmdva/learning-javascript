2c0c64b23fea61f0d06f397addbda112ac446533
Muhammad Vallen <vallenleyss@gmail.com>
2026-08-12 13:51:40 +0700

    create add fields & clear fields button

--- javascript/hospital.js
+++ javascript/hospital.js
@@ -1,4 +1,4 @@
-let totalDoctor = 4, totalPatient = 0, totalNurse = 1;
+    let totalDoctor = 4, totalPatient = 0, totalNurse = 1;
 
 const CATEGORY_PATIENT = ["A", "B", "C"];
 const QUEUE_NUMBER = { A: 0, B: 0, C: 0 };
@@ -54,8 +54,61 @@ for (let i = 0; i < registerPatient.name.length; i++) {
     );
 }
 
-const title = document.getElementById('title');
-title.style.color = "#CBA347";
-const subTitle = document.getElementById('sub-title');
-subTitle.style.color = "#CBA347";
-console.log(subTitle)
+const form = document.querySelector('.form');
+const buttonCard = document.querySelector('.btn-card');
+const addField = document.querySelector('#add-field');
+const clearField = document.querySelector('#clear-field');
+
+let isVisible = false;
+let countClick = 0;
+
+addField.addEventListener('click', function(event){    
+    event.preventDefault();
+
+    if (countClick >= 3) {
+        alert('Complaints exceeding the limit');
+        return;
+    }
+
+    countClick++;
+    clearField.classList.remove('active');
+    
+    const complaintGroup = document.createElement('div');
+    complaintGroup.classList.add('complaint-group');
+
+    const label = document.createElement('label');
+    label.textContent = "Patient Complaints " + countClick;
+    label.classList.add("label-patient-complaints")
+
+    const textArea = document.createElement('textarea');
+    textArea.classList.add('form-input', 'patient-complaints');
+    textArea.placeholder = 'please explain your complaint';
+
+    complaintGroup.append(label);
+    complaintGroup.append(textArea);    
+    
+    form.insertBefore(complaintGroup, buttonCard);
+});
+
+clearField.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const clearConfirm = confirm('Are you sure you want to clear the fields?');
+
+    if (clearConfirm) {
+        clearFields();
+        alert('Fields successfully cleared!');
+    } else {
+        alert('Fields were not cleared.');
+    }
+});
+
+function clearFields(){   
+    const complaintGroups = document.querySelectorAll('.complaint-group');
+    complaintGroups.forEach(function(group) {
+            group.remove();
+    });
+
+    countClick = 0;
+    clearField.classList.add('active');
+}
